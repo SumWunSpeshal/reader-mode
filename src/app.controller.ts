c97dc96a55d75922fd6268622874c0011b1bb51e
Sacha Rodier <mail@srodier.com>
2024-05-14 22:50:10 +0200

    implement html-parser

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, ValidationPipe } from '@nestjs/common';
+import { IsUrl } from 'class-validator';
+import parse from 'node-html-parser';
 import { AppService } from './app.service';
+
+export class PaginationRequestDto {
+  @IsUrl()
+  readonly q?: string;
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async get(
+    @Query(new ValidationPipe()) _: PaginationRequestDto,
+    @Query('q') q: string
+  ): Promise<string> {
+    const html = await fetch(q).then(r => r.text());
+    const root = parse(html);
+    const url = new URL(q);
+    console.log(url);
+
+    root.querySelectorAll('[style]').forEach(el => el.removeAttribute('style'));
+
+    // root.querySelectorAll('img').forEach(img => {
+    //   img.setAttribute('src', url.origin + img.getAttribute('src'));
+    // });
+    root.querySelector('head').remove();
+    root.querySelectorAll('header, footer, script').forEach(el => el.remove());
+
+    return root.toString();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return '<h1>Hello World!</h1>';
-  }
-}
+export class AppService {}
